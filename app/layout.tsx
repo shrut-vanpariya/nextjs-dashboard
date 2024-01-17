309f5efe95bebafa40a7b5c74d0b681cf6564841
Shrut Vanpariya <shrutvanpariya@gmail.com>
2024-01-17 22:46:42 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-import Head from 'next/head';
-
 
 export const metadata: Metadata = {
   title: {
@@ -19,10 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Include the meta tag here */}
-        <meta name="google-site-verification" content="-GsOjATdw51ONR2Wfwq0t0dmx0NM6MxjpR6f1yRiVZ4" />
-      </Head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
